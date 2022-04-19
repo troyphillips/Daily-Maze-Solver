86967ce851ca0150ccf24f19eec39f4a951a9b33
Yifan Kang <97686349+yifan-k@users.noreply.github.com>
2022-04-19 16:40:50 -0400

    Update maze.js

--- src/maze.js
+++ src/maze.js
@@ -268,7 +268,7 @@ var app = new Vue({
         var self = this
         $.getJSON('src/maze.json', function (data) {
             self.height = data.height;
-            self.weight = data.weight;
+            self.width = data.weight;
             self.mazeData = data.cells;
             self.x_c = data.start_x;
             self.y_c = data.start_y;
